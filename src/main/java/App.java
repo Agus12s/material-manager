76b489abf43efd16a93d723a3e96e911de0037f7
Agus12s <agustinaprada@gmail.com>
2018-06-18 23:56:49 -0300

    Arreglado primer commit.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,17 @@
+package main.java;
+
 import view.MainView;
-import view.NewIngredientView;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 public class App {
   public static void main(String[] args) {
     SwingUtilities.invokeLater(new Runnable() {
       @Override public void run() {
         final JFrame mainWindow = new MainView();
-        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainWindow.setVisible(true);
       }
     });
