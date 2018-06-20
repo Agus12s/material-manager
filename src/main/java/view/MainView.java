aa972d85b604b6d5fbe9997de58356fc551a3394
Agus12s <agustinaprada@gmail.com>
2018-06-20 03:52:48 -0300

    Arreglado Main View.

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -29,28 +29,21 @@ public class MainView extends JFrame {
         elementsContainer.setSize(500, 400);
         setSize(elementsContainer.getSize());
         elementsContainer.setBackground(new Color(220, 237, 255));
-        GridBagLayout gbAllContentContainer = new GridBagLayout();
-        GridBagConstraints gbcAllContentContainer = new GridBagConstraints();
-        elementsContainer.setLayout(gbAllContentContainer);
+        elementsContainer.setLayout(new GridBagLayout());
 
-        JLabel header = createHeader(gbAllContentContainer);
-        elementsContainer.add(header);
-
-        JPanel itemsContainer = createItemContainer(gbAllContentContainer);
-        elementsContainer.add(itemsContainer);
-
-        JButton addButton = createAddButton(gbAllContentContainer);
-        elementsContainer.add(addButton);
+        elementsContainer.add(createHeader(), createHeaderConstraints());
+        elementsContainer.add(createItemContainer(), createItemContainerConstraints());
+        elementsContainer.add(createAddButton() , createAddButtonConstraints());
 
         return elementsContainer;
     }
 
     /**
      * Simply creates the header. Title of the window.
-     * @param gridBagLayout Layout to set constraints from within the method.
+     *
      * @return the component for the main panel.
      */
-    private JLabel createHeader(GridBagLayout gridBagLayout) {
+    private JLabel createHeader() {
         JLabel titleLabel = new JLabel("ADMINISTRADOR DE MATERIALES E INGREDIENTES");
         titleLabel.setBackground(new Color(255, 240, 240));
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -58,18 +51,21 @@ public class MainView extends JFrame {
         titleLabel.setFont(new Font("Arial", Font.BOLD, 14));
         titleLabel.setForeground(new Color(52, 63, 62));
         titleLabel.setBorder(BorderFactory.createTitledBorder(""));
-        GridBagConstraints gbcTitle = new GridBagConstraints();
-        gbcTitle.gridx = 0;
-        gbcTitle.gridy = 0;
-        gbcTitle.gridwidth = 20;
-        gbcTitle.gridheight = 3;
-        gbcTitle.fill = GridBagConstraints.BOTH;
-        gbcTitle.weightx = 0;
-        gbcTitle.weighty = 0;
-        gbcTitle.anchor = GridBagConstraints.PAGE_START;
-        gbcTitle.insets = new Insets(5, 5, 5, 5);
-        gridBagLayout.setConstraints(titleLabel, gbcTitle);
         return titleLabel;
+    }
+
+    private GridBagConstraints createHeaderConstraints() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 20;
+        gbc.gridheight = 3;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        return gbc;
     }
 
     /**
@@ -77,50 +73,64 @@ public class MainView extends JFrame {
      *
      * @return the container, with it's main LM config.
      */
-    private JPanel createItemContainer(GridBagLayout gridBagLayout) {
-        final JPanel itemsContainer = new JPanel();
-        GridBagLayout gbcItemsContainer = new GridBagLayout();
-        GridBagConstraints gbcItemList = new GridBagConstraints();
-        GridBagLayout gbItemListContainer = new GridBagLayout();
-        GridBagConstraints gbcItemListContainer = new GridBagConstraints();
-        itemsContainer.setLayout(gbItemListContainer);
+    private JPanel createItemContainer() {
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        final JPanel itemsContainer = new JPanel(gridBagLayout);
         itemsContainer.setBorder(BorderFactory.createTitledBorder(""));
-        gbcItemList.gridx = 0;
-        gbcItemList.gridy = 3;
-        gbcItemList.gridwidth = 20;
-        gbcItemList.gridheight = 15;
-        gbcItemList.fill = GridBagConstraints.BOTH;
-        gbcItemList.weightx = 1;
-        gbcItemList.weighty = 1;
-        gbcItemList.anchor = GridBagConstraints.NORTH;
-        gbcItemList.anchor = GridBagConstraints.CENTER;
-        itemsContainer.setLayout(gbcItemsContainer);
-        gridBagLayout.setConstraints(itemsContainer, gbcItemList);
+        itemsContainer.setBackground(new Color(52, 63, 62));
         return itemsContainer;
     }
 
-    private JButton createAddButton(GridBagLayout gridBagLayout) {
+    private GridBagConstraints createItemContainerConstraints() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 20;
+        gbc.gridheight = 15;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.anchor = GridBagConstraints.NORTH;
+        return gbc;
+    }
+
+    private GridBagConstraints createItemConstrainsts() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        gbc.insets = new Insets(2, 0, 2, 0);
+        return gbc;
+    }
+
+    private JButton createAddButton() {
         JButton addButton = new JButton("Agregar");
 
         addButton.setBackground(new Color(148, 176, 218));
         addButton.setForeground(new Color(52, 63, 62));
 
-        GridBagConstraints gbcAddButton = new GridBagConstraints();
-        gbcAddButton.gridx = 0;
-        gbcAddButton.gridy = 18;
-        gbcAddButton.gridwidth = 20;
-        gbcAddButton.gridheight = 2;
-        gbcAddButton.fill = GridBagConstraints.BOTH;
-        gbcAddButton.weightx = 1;
-        gbcAddButton.weighty = 0;
-        gbcAddButton.anchor = GridBagConstraints.PAGE_END;
-        gbcAddButton.insets = new Insets(5, 5, 5, 5);
-        gridBagLayout.setConstraints(addButton, gbcAddButton);
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 new NewItemView().setVisible(true);
             }
         });
         return addButton;
+    }
+
+    private GridBagConstraints createAddButtonConstraints() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 18;
+        gbc.gridwidth = 20;
+        gbc.gridheight = 2;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1;
+        gbc.weighty = 0;
+        gbc.anchor = GridBagConstraints.PAGE_END;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        return gbc;
     }
 }
